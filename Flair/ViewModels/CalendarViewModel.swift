292c3a2569333f59faac75dc99ea2f535b4520b1
Apprenant151 <apprenant151@macbook-pro-5.home>
2026-08-14 02:04:43 +0200

    add view model and view for Calendar interface

---
+++ Flair/ViewModels/CalendarViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  CalendarViewModel.swift
+//  Flair
+//
+//  Created by Apprenant151 on 08/08/2026.
+//
+
+import Foundation
+
+struct CalendarViewModel {
+
+    let works: [Work]
+    let type: Types
+    let genre: Genre
+
+    static func dateFormatted(date: Date) -> String {
+        return "\(date.formatted(.dateTime.day(.twoDigits))) / \(date.formatted(.dateTime.month(.twoDigits)))"
+    }
+
+    static func futureWork(works: [Work]) -> [Work] {
+        works.filter { $0.airedDate > Date() }
+    }
+}
